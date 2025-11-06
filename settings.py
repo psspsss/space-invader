b071a50bbd394d830eae02ca9270decbdaa7e4fc
vinayak majhi <vinayak.djp@gmail.com>
2025-11-06 15:36:47 +0530

    updated ui

--- settings.py
+++ settings.py
@@ -1,16 +1,16 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (60, 0, 40)
 
         self.ship_speed_factor = 1.5
         self.ship_limit = 3
 
         self.bullet_speed_factor = 1
-        self.bullet_width = 2000
+        self.bullet_width = 10
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = 255, 255, 255
         self.bullets_allowed = 10
 
         self.alien_speed_factor = 0.01
